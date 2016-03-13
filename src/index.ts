c5a2b2c19beedfce79492f96e734e8505b003d61
Lea Hayes <lea@rotorz.com>
2016-03-13 20:23:17 +0000

    Throw error when attempting to import package directly.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 // Copyright (c) Rotorz Limited. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root.
 
+throw new Error("Use the `webreed` or `webreed-core` package to interact with webreed API.");
